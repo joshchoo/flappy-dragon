80f10549ff9749c8e772299c337538157b3d67cd
Josh Choo <dev@joshuous.com>
2021-05-21 10:22:40 +0800

    create game modes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
 use bracket_lib::prelude::*;
 
 /// The game state
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+
+    // TODO: 2021-05-21 implement
+    fn main_menu(&self, ctx: &mut BTerm) {}
+
+    // TODO: 2021-05-21 implement
+    fn play(&self, ctx: &mut BTerm) {}
+
+    // TODO: 2021-05-21 implement
+    fn dead(&self, ctx: &mut BTerm) {}
+}
+
+/// The GameMode state machine
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
 impl GameState for State {
     /// ctx provides functions for interacting with the game display
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
 
@@ -16,5 +45,5 @@ fn main() -> BError {
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
