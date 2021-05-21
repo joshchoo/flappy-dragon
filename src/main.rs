7930bdbf630109f5f47f695a05e230595c28d607
Josh Choo <dev@joshuous.com>
2021-05-21 10:00:10 +0800

    create game state and start game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+/// The game state
+struct State {}
+
+impl GameState for State {
+    /// ctx provides functions for interacting with the game display
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State {})
 }
