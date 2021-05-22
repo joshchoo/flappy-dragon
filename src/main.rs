a466944c4611500eae1cc2fab84db3c6294592ec
Josh Choo <dev@joshuous.com>
2021-05-22 17:45:38 +0800

    add render, flap and gravity_and_move methods

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,25 @@ impl Player {
             velocity: 0.0,
         }
     }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
 }
 
 /// The GameMode state machine
